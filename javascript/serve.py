2ae3b763ecb44801897aea7220b80ffca6f877c1
Sean DuBois <sean@pion.ly>
2018-02-23 21:46:29 -0800

    demo-conference: Add container, remove Python script

--- javascript/serve.py
+++
@@ -1,6 +0,0 @@
-import BaseHTTPServer, SimpleHTTPServer
-import ssl
-
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='../../docker/files/certificates/pionsh.pem', server_side=True, keyfile='../../docker/files/certificates/pionsh.key')
-httpd.serve_forever()
